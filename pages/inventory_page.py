071f5eba0c9151b40cf867f3008045490955d9b9
ng-qa <nesanet.ghebreslasie@gmail.com>
2026-07-19 21:02:22 +0200

    Update inventory_page.py

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -3,15 +3,17 @@ from selenium.webdriver.common.by import By
 
 class InventoryPage:
 
-    ADD_BACKPACK = (By.ID, "add-to-cart-sauce-labs-backpack")
     CART = (By.CLASS_NAME, "shopping_cart_link")
 
     def __init__(self, driver):
         self.driver = driver
 
-    def add_backpack(self):
-        self.driver.find_element(*self.ADD_BACKPACK).click()
+    def add_product(self, product_name):
+        product_button = (
+            By.XPATH,
+            f"//div[text()='{product_name}']/ancestor::div[@class='inventory_item']//button"
+        )
+        self.driver.find_element(*product_button).click()
 
     def open_cart(self):
         self.driver.find_element(*self.CART).click()
-        error_message = self.driver.find_element(By.CSS_SELECTOR, "div.cart-empty").text.replace("\u2018", "").replace("\u2019", "").strip()
