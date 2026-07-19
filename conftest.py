2b470105071a41e33cb7be91d5b9eb840d12dbf6
ng-qa <nesanet.ghebreslasie@gmail.com>
2026-07-19 21:00:43 +0200

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,20 +1,19 @@
 import pytest
-import selenium
+from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 @pytest.fixture
 def driver():
-    """
-    Fixture für einen Chrome-Driver.
     
-    :return: Ein instanziiertes WebDriver-Objekt.
-    """
-    options = selenium.webdriver.ChromeOptions()
+    options = webdriver.ChromeOptions()
+
     options.add_argument("--start-maximized")
+   
     
     service = Service(ChromeDriverManager().install())
-    driver = selenium.webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(service=service, options=options)
     
     yield driver
     driver.quit()
+
