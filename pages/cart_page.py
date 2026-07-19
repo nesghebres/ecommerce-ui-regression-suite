c505d7652a64f5d851755b46d53df299129a3499
ng-qa <nesanet.ghebreslasie@gmail.com>
2026-07-19 21:01:19 +0200

    Update cart_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -10,4 +10,3 @@ class CartPage:
 
     def checkout(self):
         self.driver.find_element(*self.CHECKOUT).click()
-        error_message = self.driver.find_element(By.CSS_SELECTOR, "div.cart-empty").text.replace("\u2018", "").replace("\u2019", "").strip()
